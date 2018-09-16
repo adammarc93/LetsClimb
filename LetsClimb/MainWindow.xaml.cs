7020699147e6f7b286a32639204da6e74c4a4a27
Adam Marciniak <adammarc93@gmail.com>
2018-09-16 15:44:38 +0200

    Fixed names

--- LetsClimb/MainWindow.xaml.cs
+++ LetsClimb/MainWindow.xaml.cs
@@ -21,8 +21,8 @@
             InitializeComponent();
             this.DataContext = this;
             CompetitorList = new ObservableCollection<Competitor>();
-            this.SexComboBox.ItemsSource = Enum.GetValues(typeof(Sex));
-            this.CategoryComboBox.ItemsSource = Enum.GetValues(typeof(Category));
+            this.CompatitorSexComboBox.ItemsSource = Enum.GetValues(typeof(Sex));
+            this.CompatitorCategoryComboBox.ItemsSource = Enum.GetValues(typeof(Category));
         }
 
         /// <summary>
@@ -32,12 +32,12 @@
         {
             try
             {
-                var name = this.NameTextBox.Text;
-                var lastName = this.LastNameTextBox.Text;
-                var sex = (Sex)Enum.Parse(typeof(Sex), this.SexComboBox.Text);
-                var category = (Category)Enum.Parse(typeof(Category), this.CategoryComboBox.Text);
-                var top = Int32.Parse(this.TopTextBox.Text);
-                var bonus = Int32.Parse(this.BonusTextBox.Text);
+                var name = this.CompatitorNameTextBox.Text;
+                var lastName = this.CompatitorLastNameTextBox.Text;
+                var sex = (Sex)Enum.Parse(typeof(Sex), this.CompatitorSexComboBox.Text);
+                var category = (Category)Enum.Parse(typeof(Category), this.CompatitorCategoryComboBox.Text);
+                var top = Int32.Parse(this.CompatitorTopTextBox.Text);
+                var bonus = Int32.Parse(this.CompatitorBonusTextBox.Text);
                 var competitor = new Competitor(name, lastName, sex, category, top, bonus);
                 CompetitorList.Add(competitor);
             }
@@ -69,12 +69,12 @@
         /// </summary>
         private void ClearTextBoxes()
         {
-            this.NameTextBox.Clear();
-            this.LastNameTextBox.Clear();
-            this.SexComboBox.SelectedIndex = -1;
-            this.CategoryComboBox.SelectedIndex = -1;
-            this.TopTextBox.Clear();
-            this.BonusTextBox.Clear();
+            this.CompatitorNameTextBox.Clear();
+            this.CompatitorLastNameTextBox.Clear();
+            this.CompatitorSexComboBox.SelectedIndex = -1;
+            this.CompatitorCategoryComboBox.SelectedIndex = -1;
+            this.CompatitorTopTextBox.Clear();
+            this.CompatitorBonusTextBox.Clear();
         }
     }
 }
